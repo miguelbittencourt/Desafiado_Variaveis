1ffd6c3db7abcb1946151e920479dbe11e435279
miguel.bittencourt <miguel.bittencourt@NTB-2KT64W2>
2022-05-16 11:04:06 -0300

    Add project files.

---
+++ Desafiado_Variaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+//Exercício 1
+float idadeMinima, idadeMaxima, idadeMedia;
+
+idadeMinima = 20;
+idadeMaxima = 50;
+idadeMedia = (idadeMinima + idadeMaxima) / 2;
+
+Console.WriteLine($"A idade média dos alunos é: {idadeMedia}");
+
+//Exercício 2
+Console.WriteLine("Entre com a cotação do euro: ");
+double cotacaoEuro = double.Parse(Console.ReadLine());
+Console.WriteLine("Entre com o valor que deseja converter de Euro para Real: ");
+double valorEuro = double.Parse(s: Console.ReadLine());
+double valorEuroReal = cotacaoEuro * valorEuro;
+Console.WriteLine($"O valor em Real é: {valorEuroReal}");
