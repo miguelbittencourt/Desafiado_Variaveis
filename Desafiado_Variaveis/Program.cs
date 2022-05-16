045bf11a0162634343e042b27cc5ebf5db3709a2
Miguel Bittencourt <miguel.bittencourt@programmers.com.br>
2022-05-16 11:31:13 -0300

    exercicios 1 e 2 completos

--- Desafiado_Variaveis/Program.cs
+++ Desafiado_Variaveis/Program.cs
@@ -1,18 +1,38 @@
-﻿
+﻿//Exercício 1
+Console.WriteLine("Exercicio 1");
+exercicio1();
 
-//Exercício 1
-float idadeMinima, idadeMaxima, idadeMedia;
+static void exercicio1()
+{
+    float idadeMinima, idadeMaxima, idadeMedia;
 
-idadeMinima = 20;
-idadeMaxima = 50;
-idadeMedia = (idadeMinima + idadeMaxima) / 2;
+    idadeMinima = 20;
+    idadeMaxima = 50;
+    idadeMedia = (idadeMinima + idadeMaxima) / 2;
 
-Console.WriteLine($"A idade média dos alunos é: {idadeMedia}");
+    Console.WriteLine($"A idade média dos alunos é: {idadeMedia}");
+}
+
 
 //Exercício 2
-Console.WriteLine("Entre com a cotação do euro: ");
-double cotacaoEuro = double.Parse(Console.ReadLine());
-Console.WriteLine("Entre com o valor que deseja converter de Euro para Real: ");
-double valorEuro = double.Parse(s: Console.ReadLine());
-double valorEuroReal = cotacaoEuro * valorEuro;
-Console.WriteLine($"O valor em Real é: {valorEuroReal}");
+Console.WriteLine("Exercicio 2");
+exercicio2();
+
+static void exercicio2()
+{
+    Console.WriteLine("Entre com a cotação do euro: ");
+    double cotacaoEuro = double.Parse(Console.ReadLine());
+    double valorEuro = 750.0;
+    double valorEuroReal = cotacaoEuro * valorEuro;
+    Console.WriteLine($"O valor de 750 euros em Real é: R${valorEuroReal}");
+};
+
+
+//Exercicio 3
+//Exercicio 4
+//Exercicio 5
+//Exercicio 6
+//Exercicio 7
+//Exercicio 8
+//Exercicio 9
+//Exercicio 10
